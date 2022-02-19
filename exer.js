fe586cf6d213265b30af512efff48dc7777f3a42
OyoCute <98724698+OyoCute@users.noreply.github.com>
2022-02-19 11:52:02 +0800

    Update exer.js

--- exer.js
+++ exer.js
@@ -1,2 +1,20 @@
-function run() {
+function run() { 
+
+document.getElementById("output").innerHTML =
+"Browser inner window width: " + window.innerWidth + "px<br>" +
+"Browser inner window height: " + window.innerHeight + "px";
 }
+
+
+// This code is used to get the width and height of the window via live interaction//
+
+// function getSize()
+// {
+
+// //Getting the value of Windows width and height
+// var w = document.documentElement.clientWidth;
+// var h = document.documentElement.clientHeight;
+        
+// // put the result into a h1 tag
+//  document.getElementById('output').innerHTML = "<p>Browser inner window width: " + w + " Browser inner window height: " + h + "</p>";
+// }
